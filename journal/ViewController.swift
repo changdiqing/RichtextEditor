39e2102fa8de994b213eb7621ef8f13560a095d6
changdiqing <changdiqing@live.cn>
2017-11-01 15:43:42 +0100

    Initial Commit

---
+++ journal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  journal
+//
+//  Created by Diqing Chang on 01.11.17.
+//  Copyright © 2017 Diqing Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
